d74fd9c00c6e7a99611e788faa483784b6edde0e
Rok Mrhar <rokmrhar9@gmail.com>
2024-11-06 11:52:52 +0100

    Add files via upload

--- script1.js
+++ script1.js
@@ -29,7 +29,7 @@ document.addEventListener("DOMContentLoaded", () => {
     showStep(currentStep);
 });
 
-document.getElementById("multiStepForm").onsubmit=function() {
+document.getElementById("multiStepForm").onsubmit = function() {
 myFunction()};
     
 function myFunction(){
